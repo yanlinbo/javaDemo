f4b992b58e8d8288cdfbb64c4a2bbdfb1f74cad0
yanlinbo <973664322@qq.com>
2022-10-18 16:19:03 +0800

    yanlinbo 20221018 用代理模式实现追求女孩

---
+++ demoTools/src/main/java/com/ylb/demoTest/designPattern/proxy/SchoolGirl.java
@@ -0,0 +1,13 @@
+package com.ylb.demoTest.designPattern.proxy;
+
+public class SchoolGirl {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
